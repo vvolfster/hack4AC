5d2ea9f8df8e42cfb503de0ff4cd42aea9ce74cc
Jason <jev4zs@virginia.edu>
2018-04-07 11:10:47 -0700

    creating the table

--- src/pages/supplies/supplydetail/index.vue
+++ src/pages/supplies/supplydetail/index.vue
@@ -1,5 +1,9 @@
 <template>
   <q-page class="flex flex-center">
+    <q-table
+        title="Supply Dashboard"
+        :columns="columns"
+    />
 
   </q-page>
 </template>
@@ -9,11 +13,38 @@
 
 <script>
 export default {
-    name: 'PageIndex',
-    data() {
-        return {
-            zsubscriptions: ["org/egan"]    // subscribe to this path in the database. CHANGES ARE REALTIME.
-        }
-    }
+    name: 'SupplyDetail',
+    data: () => ({
+        columns: [
+            {
+                name: 'name',
+                required: true,
+                label: 'Item',
+                align: 'left',
+                field: 'name',
+                sortable: true
+            },
+            {
+                name: 'qty',
+                required: true,
+                label: 'Quantity',
+                align: 'left',
+                field: 'qty',
+                sortable: true
+            },
+            {
+                name: 'fulfilled',
+                required: true,
+                label: 'Fulfilled?',
+                align: 'left',
+                field: 'fulfilled',
+                sortable: true
+            }
+        ]
+    })
 };
 </script>
+/*        return {
+            zsubscriptions: ["org/egan"]    // subscribe to this path in the database. CHANGES ARE REALTIME.
+        }
+*/
