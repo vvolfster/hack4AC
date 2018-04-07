36ec78ff5b2b528c9c7d54be1b72882fd2c0f10f
Yves <yves.gurcan@emberex.com>
2018-04-07 09:58:06 -0700

    boilerplate of site-details

--- src/pages/sites/detail/index.vue
+++ src/pages/sites/detail/index.vue
@@ -4,12 +4,54 @@
         suppliesNeeded
         incidents
         userCard
+        <pre>
+          {{ site }}
+        </pre>
   </q-page>
 </template>
 
 <script>
 export default {
     name: 'PageIndex',
+    // components: {
+
+    // },
+    props: [""],
+    data() {
+        return {
+            zsubscriptions: ["org/egan"],
+            id: "a"
+        };
+    },
+    computed: {
+        site() {
+            try{
+                return this.zsubData['org/egan'].site[this.id];
+            }catch(e){
+                console.error('failed', e)
+                return {}
+            }
+        }
+    },
+    created() {},
+    mounted() {
+    // const self = this;
+    // this.$nextTick(() => {
+    //     try {
+    //     } catch (e) {
+    //         //
+    //     }
+    // });
+    },
+    methods: {
+        getPercent(site) {
+            const x = site.guest.current / site.guest.max
+            return x * 100;
+        },
+        clickSite(site) {
+            console.log("clicked site", site);
+        }
+    }
 };
 </script>
 
