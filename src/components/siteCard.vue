0e84dd44893f207c526310db69eb10f373e2ae96
Brett Ansite <brett@Bretts-MacBook-Pro.local>
2018-04-07 08:56:18 -0700

    site card template

--- src/components/siteCard.vue
+++ src/components/siteCard.vue
@@ -1,12 +1,55 @@
 <template>
   <div>
-      Your HTML here
+      <q-item @click.native="clickSite(site)">
+        <q-item-side>
+        </q-item-side>
+        <q-item-main :label="site.title">
+            <div>
+                <q-progress :percentage="getPercent(site)"
+                      style="height: 4px" />
+                {{moment(site.guest.lastUpdated).fromNow()}}
+            </div>
+
+        </q-item-main>
+        <q-item-side right>
+          <q-item-tile icon="person"
+                       color="green" /> {{site.guest.current}}/{{site.guest.max}}
+          <q-item-tile icon="directions car"
+                       color="green" /> {{site.suppliesNeeded.length}}
+        </q-item-side>
+        <q-item-separator />
+      </q-item>
   </div>
 </template>
 
 <script>
 export default {
-    // your script code here
+    name: "",
+    components: {},
+    props: ["site"],
+    data() {
+        return {
+        };
+    },
+    computed: {
+
+    },
+    created() {},
+    mounted() {
+        console.log("site", this.site)
+    // const self = this;
+    // this.$nextTick(() => {
+    //     try {
+    //     } catch (e) {
+    //         //
+    //     }
+    // });
+    },
+    methods: {
+        getPercent(site){
+            return (site.guest.current / site.guest.max) * 100
+        },
+    }
 }
 </script>
 
