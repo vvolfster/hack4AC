3be4417651f784db07bb304c9bc3df7245df8407
Wolf <shahan@zabaat.com>
2018-04-07 13:18:48 -0700

    fix

--- src/pages/supplies/supplydetail/index.vue
+++ src/pages/supplies/supplydetail/index.vue
@@ -1,20 +1,24 @@
 <template>
   <q-page class='flex'>
-    <q-table
-        class="supplytable"
-        title='Supply Dashboard'
-        row-key='name'
-        :columns="columns"
-        :data="tableData"
-        :selection="selection"
-        :selected.sync="selected"
-    >
-        <template slot="top-selection" slot-scope="props">
-            <q-btn color="secondary" flat label="Fulfill" />
-            <div class="col" />
-            <q-btn color="negative" flat round delete icon="delete" @click="deleteRow" />
-        </template>
-    </q-table>
+        <div v-if="!computedData">
+            Still loading
+        </div>
+        <q-table
+            v-else
+            class="supplytable"
+            title='Supply Dashboard'
+            row-key='name'
+            :columns="computedData.columns"
+            :data="computedData.tableData"
+            :selection="computedData.selection"
+            :selected.sync="computedData.selected"
+        >
+            <template slot="top-selection" slot-scope="props">
+                <q-btn color="secondary" flat label="Fulfill" />
+                <div class="col" />
+                <q-btn color="negative" flat round delete icon="delete" @click="deleteRow" />
+            </template>
+        </q-table>
 
   </q-page>
 </template>
@@ -29,42 +33,47 @@
 export default {
     name: 'SupplyDashboard',
     props: [""], // TODO siteID should go here
-    data: () => ({
-        siteId: "a",
-        zsubscriptions: [`org/egan/site/${this.siteId}`],
-        columns: [
-            {
-                name: 'name',
-                required: true,
-                label: 'Item',
-                align: 'left',
-                field: 'name',
-                sortable: true
-            },
-            {
-                name: 'qty',
-                required: true,
-                label: 'Quantity',
-                align: 'left',
-                field: 'qty',
-                sortable: true
-            },
-            {
-                name: 'fulfilled',
-                required: true,
-                label: 'Fulfilled?',
-                align: 'left',
-                field: 'fulfilled',
-                sortable: true
-            }
-        ],
-        tableData: this.currentSite.suppliesNeeded,
-        selection: 'multiple',
-        selected: [
-
-        ]
-    }),
     computed: {
+        computedData() {
+            if(!this.currentSite)
+                return null
+
+            return {
+                siteId: "a",
+                zsubscriptions: [`org/egan/site/a`],
+                columns: [
+                    {
+                        name: 'name',
+                        required: true,
+                        label: 'Item',
+                        align: 'left',
+                        field: 'name',
+                        sortable: true
+                    },
+                    {
+                        name: 'qty',
+                        required: true,
+                        label: 'Quantity',
+                        align: 'left',
+                        field: 'qty',
+                        sortable: true
+                    },
+                    {
+                        name: 'fulfilled',
+                        required: true,
+                        label: 'Fulfilled?',
+                        align: 'left',
+                        field: 'fulfilled',
+                        sortable: true
+                    }
+                ],
+                tableData: this.currentSite.suppliesNeeded,
+                selection: 'multiple',
+                selected: [
+
+                ]
+            }
+        },
         currentSite() {
             try {
                 return this.zsubData[`org/egan/site/${this.siteId}`];
