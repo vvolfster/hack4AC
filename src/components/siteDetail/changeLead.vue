a613744cf51f017609ac93475f775a3ee78b855a
Yves <yves.gurcan@emberex.com>
2018-04-07 18:38:20 -0700

    pull from master

---
+++ src/components/siteDetail/changeLead.vue
@@ -0,0 +1,31 @@
+<template>
+    <div id="root" class="q-pa-md">
+        <q-toggle :value="checkIfCurrentUserIsOnSite"
+                @input="() => toggleLead()"
+                :label="'Take ' + role" />
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'changeLead',
+    components: {},
+    props: ['role'],
+    data() {
+        return {};
+    },
+    computed: {},
+    created() {},
+    mounted() {},
+    methods: {
+        toggleLead() {
+            // TODO/FIXME: add server request to change lead
+
+        }
+    },
+};
+</script>
+
+<style scoped>
+
+</style>
