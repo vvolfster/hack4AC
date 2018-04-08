88b39963aad53724001f126e873f1ea45b49a00d
Yves <yves.gurcan@emberex.com>
2018-04-07 18:12:04 -0700

    supplies needed

--- src/components/siteDetail/supply.vue
+++ src/components/siteDetail/supply.vue
@@ -1,23 +1,67 @@
 <template>
-  <div id="root" class="q-pa-sm">
-
+  <div id="root"
+       class="q-pa-sm">
+    <q-table class="supplytable"
+             title="Supplies Needed"
+             row-key='name'
+             :columns="columns"
+             :data="suppliesNeeded">
+      <template slot="top-selection"
+                slot-scope="props">
+        <q-btn color="secondary"
+               flat
+               label="Add Item" />
+        <q-btn color="secondary"
+               flat
+               label="Fulfill" />
+        <div class="col" />
+        <q-btn color="negative"
+               flat
+               round
+               delete
+               icon="delete"
+               @click="deleteRow" />
+      </template>
+    </q-table>
   </div>
 </template>
 
 <script>
 export default {
-    name: "supplies",
+    name: 'supplies',
     components: {},
-    props: ["siteId"],
+    props: ['readOnly', 'suppliesNeeded'],
     data() {
         return {};
     },
-    computed: {},
+    computed: {
+        columns() {
+            return [
+                {
+                    name: 'name',
+                    required: true,
+                    label: 'Item',
+                    align: 'left',
+                    field: 'name',
+                    sortable: true,
+                },
+                {
+                    name: 'qty',
+                    required: true,
+                    label: 'Quantity',
+                    align: 'left',
+                    field: 'qty',
+                    sortable: true,
+                },
+            ];
+        },
+    },
     created() {},
     mounted() {},
-    methods: {}
+    methods: {},
 };
 </script>
 
 <style scoped>
+
 </style>
--- src/pages/sites/detail/index.vue
+++ src/pages/sites/detail/index.vue
@@ -9,50 +9,57 @@
             </q-btn>
         </div>
       <div>
-        <q-toggle :value="checkIfCurrentUserIsOnSite"
+        <q-toggle v-if="site.active" :value="checkIfCurrentUserIsOnSite"
                   @input="() => toggleOnSite()"
                   label="I'm on site" />
       </div>
     </div>
 
-    <h3 class="text-center">{{ site.title }}</h3>
-
-    <div>
-      <people-bar :site=site></people-bar>
-      <pet-bar :site=site></pet-bar>
+    <div v-if="!site.active">
+        <h3 class="text-center">Loading...</h3>
     </div>
 
-    <div>
-      <phone-contact role="Site Lead"
-                     :contact=site.siteLead></phone-contact>
-      <phone-contact role="Shift Lead"
-                     :contact=site.shiftLead></phone-contact>
-    </div>
+    <div v-else>
+        <h3 class="text-center">{{ site.title }}</h3>
 
-    <!-- show detailed supply requested list -->
-    <div v-if="!currentUserIsOnSite">
-    </div>
+        <div>
+        <people-bar :site=site></people-bar>
+        <pet-bar :site=site></pet-bar>
+        </div>
 
-    <!-- show full extent of functionalities, but do not show details of supply requests -->
-    <div v-if="currentUserIsOnSite">
-      <div class="flex flex-center">
-        <intake :siteId=siteId></intake>
-      </div>
+        <div>
+        <phone-contact role="Site Lead"
+                        :contact=site.siteLead></phone-contact>
+        <phone-contact role="Shift Lead"
+                        :contact=site.shiftLead></phone-contact>
+        </div>
 
-      <div class="flex flex-center">
-        <q-btn @click="$router.replace('/supplydetail/' + siteId)">
-          Request Supplies
-        </q-btn>
-      </div>
+        <!-- show detailed supply requested list -->
+        <div v-if="!currentUserIsOnSite">
+            <supply readOnly=true :suppliesNeeded=site.suppliesNeeded></supply>
+        </div>
 
-      <div class="flex flex-center">
-        <incident :siteId=siteId></incident>
-      </div>
+        <!-- show full extent of functionalities, but do not show details of supply requests -->
+        <div v-if="currentUserIsOnSite">
+        <div class="flex flex-center">
+            <intake :siteId=siteId></intake>
+        </div>
 
-      <div class="flex flex-center">
-        <changeSiteLead :siteId=siteId></changeSiteLead>
-        <changeShiftLead :siteId=siteId></changeShiftLead>
-      </div>
+        <div class="flex flex-center">
+            <q-btn @click="$router.replace('/supplydetail/' + siteId)">
+            Request Supplies
+            </q-btn>
+        </div>
+
+        <div class="flex flex-center">
+            <incident :siteId=siteId></incident>
+        </div>
+
+        <div class="flex flex-center">
+            <changeSiteLead :siteId=siteId></changeSiteLead>
+            <changeShiftLead :siteId=siteId></changeShiftLead>
+        </div>
+        </div>
     </div>
 
     <pre>
