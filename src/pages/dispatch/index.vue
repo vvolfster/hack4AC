12e1dd6759a5dcf365e713681ee6383adeae8954
Brett Ansite <brett@zabaat.com>
2018-04-08 07:32:49 -0700

    routes

--- src/pages/dispatch/index.vue
+++ src/pages/dispatch/index.vue
@@ -36,7 +36,7 @@ export default {
     computed: {
         sites() {
             try {
-                console.log('sub data', this.zsubData['org/egan']);
+                // console.log('sub data', this.zsubData['org/egan']);
                 return this.zsubData['org/egan'].site;
             } catch (e) {
                 return {};
